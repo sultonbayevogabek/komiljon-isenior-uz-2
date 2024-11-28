250488df9e693e4d6e51d57cb9063a247f83a59e
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-11-28 11:20:48 +0500

    commit

--- public/js/excel.js
+++ public/js/excel.js
@@ -11,7 +11,7 @@ if (user && user?.name && user?.phone && user?.time) {
     formData.append('Ismi', user?.name);
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
-    fetch('https://script.google.com/macros/s/AKfycbyw9egR7-12cZMmgZHQmEtfAxKo6pJ8ISrR7D-MHOuXMsomPOxHSjYRe92THBNAwKRE/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbx-c_vkQkcUEnU7-Yzg2azHAtuD7EER4habJuJuVlRk8TgjFdHgcoYkiyDPwv7inmoH/exec', {
         method: 'POST',
         body: formData
     })
